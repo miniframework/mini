77163fb2bc0e2aa0cae3f25c0be0a10fa0a365a6
inhell <hahazhi@gmail.com>
2012-07-20 22:33:58 +0800

    add cookie and session file

---
+++ http/session.class.php
@@ -0,0 +1,14 @@
+<?php
+class mini_http_session extends mini_base_component
+{
+    
+    public function __construct()
+    {
+        
+    }
+    public function init()
+    {
+        
+    }
+    
+}
